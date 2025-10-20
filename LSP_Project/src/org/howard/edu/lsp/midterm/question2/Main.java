266c8d7155222cdc6fd8fdbeca2f23dab5d594c7
Game_Neek <malikjohn74@gmail.com>
2025-10-20 19:12:23 -0400

    I added an area that would cause the Illegal argument exception but now need to implement throw.

--- LSP_Project/src/org/howard/edu/lsp/midterm/question2/Main.java
+++ LSP_Project/src/org/howard/edu/lsp/midterm/question2/Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println("Rectangle 5.0 x 2.0 → area = " + AreaCalculator.area(5.0, 2.0));
         System.out.println("Triangle base 10, height 6 → area = " + AreaCalculator.area(10, 6));
         System.out.println("Square side 4 → area = " + AreaCalculator.area(4));
+        
+        System.out.println("Circle radius -8 → area = " + AreaCalculator.area(-8));
     }
 }
